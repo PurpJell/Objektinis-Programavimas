63839db2d466217b6a0cc00ea16af861152e7735
PurpJell <115624117+PurpJell@users.noreply.github.com>
2023-03-24 15:12:00 +0200

    v0.4 -> v0.4.1

--- op.h
+++ op.h
@@ -16,20 +16,15 @@ struct studentas
 double vid(studentas &temp);
 double med(studentas &temp);
 void pildyti(studentas &temp, char md);
-void nuskaityti(vector<studentas> &mas, int &stCount);
-void spausdinti(studentas &temp, char mode);
-bool swapNames(string v1, string v2);
-void rusiuoti(vector<studentas> &mas);
+void nuskaityti(vector<studentas> &mas, int &stCount, int &pazNr);
 void generuoti(vector<studentas> &mas, int &stCount, int &pazNr);
-void kietiOut(studentas &kietas, int pazNr);
-void prastiOut(studentas &prastas, int pazNr);
-void rusiuotiGen(vector<studentas> &mas);
 void dalinti(studentas temp, vector<studentas> &kieti, vector<studentas> &prasti, int &kietiCnt, int &prastiCnt);
 int partition(vector<studentas>& mas, int start, int end);
 void quicksort(vector<studentas>& mas, int start, int end);
 void print(vector<studentas> &mas, char mode);
 void kout(vector<studentas> &kieti, int pazNr);
 void pout(vector<studentas> &prasti, int pazNr);
+bool daugiau(string s1, string s2);
 
 #endif
 
